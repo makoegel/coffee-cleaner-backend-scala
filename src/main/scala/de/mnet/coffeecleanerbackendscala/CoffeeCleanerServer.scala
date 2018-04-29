63760cb9250aa0d1ae43894c48498be01ad4abc9
makoegel <marion-koegel@gmx.de>
2018-04-29 23:20:20 +0200

    init project and http4s server

---
+++ src/main/scala/de/mnet/coffeecleanerbackendscala/CoffeeCleanerServer.scala
@@ -0,0 +1,24 @@
+package de.mnet.coffeecleanerbackendscala
+
+import cats.effect.{Effect, IO}
+import fs2.StreamApp
+import org.http4s.server.blaze.BlazeBuilder
+
+import scala.concurrent.ExecutionContext
+
+object CoffeeCleanerServer extends StreamApp[IO] {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def stream(args: List[String], requestShutdown: IO[Unit]) = ServerStream.stream[IO]
+}
+
+object ServerStream {
+
+  def coffeeCleanerService[F[_]: Effect] = new CoffeeCleanerService[F].service
+
+  def stream[F[_]: Effect](implicit ec: ExecutionContext) =
+    BlazeBuilder[F]
+      .bindHttp(8080, "0.0.0.0")
+      .mountService(coffeeCleanerService, "/")
+      .serve
+}
